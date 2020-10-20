1a18fba94d31fba0f0b9de6e2d6c663fe96a220a
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2020-10-20 20:42:38 -0300

    Start JavaScript 20/10/2020

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,1 +1,13 @@
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const map = L.map('mapid').setView([51.505, -0.09], 13);//map pego do Leaflet
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+/*adicionando uma " tilelayer -br(camada)" para o 'map' da const .addTo(map) . Foi passado uma
+um paramentro e dentro desse parametro tem um objeto que é attribution*/
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup(); 
+/* adicionando um marcador em um parametro pegando elemento dois valores
+dentro de um array ([51.5, -0.09]) e adicionando ão map, .addTo(map)*/ 
